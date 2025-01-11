a5236d9e2e337f717d949136ba0aa0807ba60e91
Inncent <innokentiykim90@gmail.com>
2025-01-11 18:32:45 +0500

    initial migrations added

--- my_shop/myshop/myshop/settings.py
+++ my_shop/myshop/myshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_USER', 'my_shop'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_USER', 'postgres'),
+        'HOST': os.getenv('DB_USER', 'localhost'),
+        'PORT': os.getenv('DB_USER', 5432),
     }
 }
 
