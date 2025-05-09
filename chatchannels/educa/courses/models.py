b55d7173f252f19e34acbbd37f34aeb9e4e544bd
inncent <innokentiykim90@gmail.com>
2025-05-09 23:14:21 +0500

    Content model updated

--- chatchannels/educa/courses/models.py
+++ chatchannels/educa/courses/models.py
@@ -46,7 +46,9 @@ class Module(models.Model):
 
 class Content(models.Model):
     module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={'model__in': (
+        'text', 'file', 'image', 'video'
+    )})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
     order = OrderField(blank=True, for_fields=['module'])
