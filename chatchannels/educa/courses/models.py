a8942557fb28907280db82b9b2b63385a5265cd0
inncent <innokentiykim90@gmail.com>
2025-05-10 00:08:19 +0500

    urls updated, base template added

--- chatchannels/educa/courses/models.py
+++ chatchannels/educa/courses/models.py
@@ -35,7 +35,7 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    order = OrderField(blank=True, for_fields='course')
+    order = OrderField(blank=True, for_fields=['course'])
 
     class Meta:
         ordering = ['order']
