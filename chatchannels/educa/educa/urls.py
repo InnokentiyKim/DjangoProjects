723def2179e06ff3a8200dfc183fad99b1d4da51
inncent <innokentiykim90@gmail.com>
2025-05-11 23:39:01 +0500

    ManageCourse view added

--- chatchannels/educa/educa/urls.py
+++ chatchannels/educa/educa/urls.py
@@ -18,12 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
-    path('accounts/login/', auth_views.LogoutView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
 
